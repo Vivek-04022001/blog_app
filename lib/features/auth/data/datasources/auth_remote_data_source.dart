080d138325bbfd599c0a684cc337419ddc4e01ec
Vivek <vk285090@gmail.com>
2025-11-03 13:45:22 +0530

    added supabase and signup data layer implemented

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,50 @@
+import 'package:blog_app/core/error/exceptions.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) async {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        email: email,
+        password: password,
+        data: {'name': name},
+      );
+      if (response.user == null) {
+        throw ServerException('Failed to sign up user');
+      }
+      return response.session!.user.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
